dccf3a7cb56be5ba0979b429d1cb57d78a956d5c
Robin Kehl <robin.kehl@singular-it.de>
2023-03-15 17:30:09 +0100

    ci: fix upsi v2

--- tests/tests/settings.py
+++ tests/tests/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     },
     DB_MYSQL: {
         "ENGINE": "django.db.backends.mysql",
-        "HOST": env.str("MYSQL_HOST", "localhost"),
+        "HOST": env.str("MYSQL_HOST", "127.0.0.1"),
         "PORT": env.int("MYSQL_PORT", 33061),
         "NAME": env.str("MYSQL_DATABASE", "test_db"),
         "USER": env.str("MYSQL_USER", "test_user"),
