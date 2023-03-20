8cac4c7db6b21fb6fd6b787e50222bf2129e9704
Robin Kehl <robin.kehl@singular-it.de>
2023-03-20 16:19:03 +0100

    ci: change postgres DB

--- tests/tests/settings.py
+++ tests/tests/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": env.str("POSTGRES_HOST"),
         "PORT": env.int("POSTGRES_PORT"),
-        "NAME": env.str("POSTGRES_DATABASE"),
+        "NAME": env.str("POSTGRES_DB"),
         "USER": env.str("POSTGRES_USER"),
         "PASSWORD": env.str("POSTGRES_PASSWORD"),
     },
