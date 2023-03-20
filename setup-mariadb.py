bb5baccb06d85035a6067c8e18b126a05ac49f6e
Robin Kehl <robin.kehl@singular-it.de>
2023-03-20 17:32:50 +0100

    ci: kp

--- setup-mariadb.py
+++ setup-mariadb.py
@@ -2,11 +2,7 @@ import subprocess
 import os
 
 
-ping = subprocess.run(['mysqladmin', 'ping', '-h', os.environ['MARIADB_HOST'], '-u', os.environ['MARIADB_USER'], '-p' + os.environ['MARIADB_PASSWORD'],
-                      capture_output=True)
-
-
-if ping.returncode == 0:
+if os.environ.get("MARIADB_HOST",None):
     print('MariaDB-Server setup...')
     if os.environ['MARIADB_ALLOW_EMPTY_PASSWORD'] == 'yes':
         cmd = f'mysql -u {MARIADB_ROOT_USERNAME} -h {MARIADB_HOST} -P {MARIADB_PORT} -e "GRANT ALL on *.* to {MARIADB_USER:-test_user};"'
--- setup-mysql.py
+++ setup-mysql.py
@@ -1,12 +1,7 @@
 import subprocess
 import os
 
-
-ping = subprocess.run(['mysqladmin', 'ping', '-h', os.environ['MYSQL_HOST'], '-u', os.environ['MYSQL_USER'], '-p' + os.environ['MYSQL_PASSWORD'],
-                      capture_output=True)
-
-
-if ping.returncode == 0:
+if os.environ.get("MYSQL_HOST",None):
     print('MySQL-Server setup...')
     if os.environ['MYSQL_ALLOW_EMPTY_PASSWORD'] == 'yes':
         cmd = f'mysql -u {MYSQL_ROOT_USERNAME} -h {MYSQL_HOST} -P {MYSQL_PORT} -e "GRANT ALL on *.* to {MYSQL_USER:-test_user};"'
