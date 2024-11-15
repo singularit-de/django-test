955e4fe4e0decfff5b108006fd73dca33bdf3e85
Robin Kehl <robin.kehl@singular-it.de>
2024-11-15 12:40:02 +0100

    test(mariadb_10): add correct db router

--- tests/app_mariadb_10/router.py
+++ tests/app_mariadb_10/router.py
@@ -5,4 +5,4 @@ from tests.base_db_router import BaseDBRouter
 
 class DBRouter(BaseDBRouter):
     route_app_labels = {"app_mariadb_10"}
-    db = settings.DB_MARIADB
+    db = settings.DB_MARIADB_10
